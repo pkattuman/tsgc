9077628da92d1910ccf3f5412a651490d6126eca
mashby22 <mwa22@cam.ac.uk>
2025-06-19 15:36:55 +0100

    Delete forecast peak functions
    Also delete associated tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -35,22 +35,3 @@ test_that("Test argmax", {
     str(zoo::index(x)[1])
   )
 })
-
-test_that("forecast.peak produces error when gamma not negative", {
-  expect_error(forecast.peak(delta = -4, gamma = 0))
-})
-
-test_that("forecast.peak produces expected numerical output", {
-  expect_equal(3, round(forecast.peak(delta = -2, gamma = -0.1)))
-})
-
-test_that("forecast_peak works correctly", {
-  #skip() # remove!
-  data(england, package = 'tsgc')
-  dat <- england$cum_cases[1:100]
-  model <- SSModelDynamicGompertz$new(Y = dat, xpred = NULL)
-  res <- model$estimate()
-  x <- forecast_peak(res$output)
-  y <- forecast.peak(-6.019327,-0.035263)
-  expect_equal(round(x),round(y))
-})
