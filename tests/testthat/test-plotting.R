cc822b3d5fa889e92b33fcad3860623b122a9cfc
mashby22 <mwa22@cam.ac.uk>
2025-06-20 17:56:23 +0100

    Update test-plotting.R

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -2,30 +2,24 @@ test_that("Test plot_new_cases() works", {
   data(gauteng, package = "tsgc")
   model <- tsgc::SSModelDynamicGompertz$new(Y = gauteng[1:50], q = 0.005)
   res <- model$estimate()
-  tsgc::plot_new_cases(
-    res, Y=gauteng, n.ahead=14, confidence.level=0.68, date_format="%Y-%m-%d"
-  )
-
+  tsgc::plot_new_cases(res)
+  expect_equal(1,1)
 })
 
-
-test_that("Test plot_forecast() works", {
+test_that("Test plot_log_forecast() works", {
   data(gauteng, package = "tsgc")
   model <- tsgc::SSModelDynamicGompertz$new(Y = gauteng[1:50], q = 0.005)
   res <- model$estimate()
-  tsgc::plot_forecast(
-    res=res, y.eval = tsgc::df2ldl(gauteng[50:65]), n.ahead = 14,
-    caption = 'hello', title = 'test'
-  )
+  tsgc::plot_log_forecast(res, gauteng)
+  expect_equal(1,1)
 })
-
 
 test_that("Test plot_gy_components() works", {
   data(gauteng, package = "tsgc")
   model <- tsgc::SSModelDynamicGompertz$new(Y = gauteng[1:50], q = 0.005)
   res <- model$estimate()
-  # Plot filtered gy, g and gamma
-  plot_gy_components(res)
+  tsgc::plot_gy_components(res)
+  expect_equal(1,1)
 })
 
 
@@ -33,7 +27,8 @@ test_that("Test plot_gy_ci() works", {
   data(gauteng, package = "tsgc")
   model <- tsgc::SSModelDynamicGompertz$new(Y = gauteng[1:50], q = 0.005)
   res <- model$estimate()
-  plot_gy_ci(res)
+  tsgc::plot_gy_ci(res)
+  expect_equal(1,1)
 })
 
 
@@ -41,10 +36,9 @@ test_that("Test plot_holdout() works", {
   data(gauteng, package = "tsgc")
   model <- tsgc::SSModelDynamicGompertz$new(Y = gauteng[1:50], q = 0.005)
   idx.est <- zoo::index(gauteng) <= as.Date("2020-07-20")
-  idx.eval <- (zoo::index(gauteng) >= as.Date("2020-07-20")) &
-    zoo::index(gauteng) <= as.Date("2020-07-27")
   model <- SSModelDynamicGompertz$new(Y = gauteng[idx.est], q = 0.005)
   res <- model$estimate()
   # Plot forecasts and outcomes over evaluation period
-  plot_holdout(res = res, Y = gauteng[idx.est], Y.eval = gauteng[idx.eval])
+  plot_holdout(res = res, Y = gauteng)
+  expect_equal(1,1)
 })
